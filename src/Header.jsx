b96d0ba880e5c70e129e04f8fa736fdd0cffeb1a
Teri P <psegiannakis@outlook.com>
2024-03-21 11:16:08 +1100

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header-div">
+        <div className="header">
+          <h2>Home</h2>
+          <h2>About</h2>
+          <h2>Portfolio</h2>
+          <h2>Contact</h2>
+        </div>
+      </div>
+    </>
+  );
+}
