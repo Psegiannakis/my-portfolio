0d6c7cdca779521da585afbd77c5f2eba808f897
Teri P <psegiannakis@outlook.com>
2024-04-09 14:32:22 +1000

    read me update

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -17,12 +17,11 @@ export default function Hero(props) {
         <div className="hero-about">
           <h3>About</h3>
           <p className="para">
-            As an aspiring web developer, I'm passionate about crafting
-            captivating digital experiences. With a focus on design and
-            expertise in the latest development tools, I'm dedicated to building
-            user-friendly websites and applications. My portfolio showcases my
-            commitment to innovation and quality craftsmanship in the digital
-            realm.
+            As a web developer, I'm passionate about crafting captivating
+            digital experiences. With a focus on design and expertise in the
+            latest development tools, I'm dedicated to building user-friendly
+            websites and applications. My portfolio showcases my commitment to
+            innovation and quality craftsmanship in the digital realm.
           </p>
         </div>
       </main>
